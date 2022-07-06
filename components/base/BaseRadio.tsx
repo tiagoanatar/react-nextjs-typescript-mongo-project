3e5d09948416b92d5dd37f3c2d56aa410a505d89
Tiago Tavares <linktiago@gmail.com>
2022-07-05 22:12:11 -0300

    Create radio effect

--- components/base/BaseRadio.tsx
+++ components/base/BaseRadio.tsx
@@ -1,35 +1,44 @@
-import { useRef, useState, useEffect } from "react";
+import { useRef, useState, useEffect, Dispatch, SetStateAction } from "react";
 
 interface Radio {
-  name: string;
-  label: string;
-  id: string;
-  value: string;
+  name: string
+  label: string
+  id: string
+  value: string
   alternate?: boolean
+  effectValue: string | undefined
+  effectFunction: Dispatch<SetStateAction<string | undefined>>
 }
 
 const defaultProps = {
   alternate: false
 }
 
-export const BaseRadio = ({ name, label, id, value, alternate = false }: Radio) => {
-  const input = useRef(null);
+export const BaseRadio = ({ name, label, id, value, alternate = false, effectValue, effectFunction }: Radio) => {
+  const input = useRef<HTMLInputElement>(null);
   const [data, setData] = useState({ checked: false });
 
   const classes = [
     'theme-radio',
     data.checked ? 'theme-radio-checked' : '',
-    alternate ? 'alt-true' : 'alt-false',
+    alternate ? 'theme-radio-alt-true' : 'theme-radio-alt-false',
   ]
 
   const radioCheck = () => {
     setData({...data, checked: !data.checked});
-    //input.current.checked = !input.current.checked
-    console.log(input.current?.value);
+    effectFunction(input.current?.value)
   };
 
+  useEffect(() => {
+    if (effectValue === input.current?.value){
+      setData({...data, checked: true});
+    } else {
+      setData({...data, checked: false});
+    }
+  }, [effectValue])
+
   return (
-    <div className={classes.join(' ')} onClick={() => radioCheck()}>
+    <div className={classes.join(' ')} onClick={() => effectFunction(input.current?.value)}>
       <input
         type="radio"
         id={id}
@@ -37,7 +46,6 @@ export const BaseRadio = ({ name, label, id, value, alternate = false }: Radio) 
         value={value}
         ref={input}
         checked={data.checked}
-        onChange={() => radioCheck()}
       />
       <label htmlFor={id}>{label}</label>
     </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,56 @@
-import type { NextPage } from 'next';
-import Head from 'next/head';
-import Image from 'next/image';
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import { useState } from "react";
 //import styles from '../styles/Home.module.scss';
 
-import { BaseButton } from '../components/base/BaseButton';
-import { BaseRadio } from '../components/base/BaseRadio';
-import { LayoutHeader } from '../components/LayoutHeader';
+import { BaseButton } from "../components/base/BaseButton";
+import { BaseRadio } from "../components/base/BaseRadio";
+import { LayoutHeader } from "../components/LayoutHeader";
 
 const Home: NextPage = () => {
+
+  const [radioData, setRadioData] = useState<string | undefined>('')
+
   return (
     <div>
-      <LayoutHeader blockA={<BaseButton label="Header Button" type="button" />} blockB="Top Text" />
+      <LayoutHeader
+        blockA={<BaseButton label="Header Button" type="button" />}
+        blockB="Top Text"
+      />
 
       <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
 
+      {radioData}
       <form>
-        <BaseRadio label="Radio Text 01" name="radio-set-01" value="radio value 1" id='radio-01' />
-        <BaseRadio label="Radio Text 01" name="radio-set-01" value="radio value 2" id='radio-02' />
-        <BaseRadio label="Radio Text 01" name="radio-set-01" value="radio value 2" id='radio-03' />
+        <BaseRadio
+          label="Radio Text 01"
+          name="radio-set-01"
+          value="radio value 1"
+          id="radio-01"
+          alternate={true}
+          effectValue={radioData}
+          effectFunction={setRadioData}
+        />
+        <BaseRadio
+          label="Radio Text 02"
+          name="radio-set-01"
+          value="radio value 2"
+          id="radio-02"
+          effectValue={radioData}
+          effectFunction={setRadioData}
+        />
+        <BaseRadio
+          label="Radio Text 03"
+          name="radio-set-01"
+          value="radio value 3"
+          id="radio-03"
+          effectValue={radioData}
+          effectFunction={setRadioData}
+        />
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
