86f290b14654023d600c9c722a2829f0a5343d95
Tiago Tavares <linktiago@gmail.com>
2022-08-07 13:11:47 -0300

    Install boostrap

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,17 @@
 import '../styles/_main.scss';
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  
+  const router = useRouter();
+  useEffect(() => {
+    typeof document !== undefined
+      ? require("bootstrap/dist/js/bootstrap")
+      : null;
+  }, []);
+
   return <Component {...pageProps} />
 }
 
